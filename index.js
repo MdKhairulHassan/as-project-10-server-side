a3e8cf7f49f1f257c70c4553f71af098158292a9
MdKhairulHassan <darknessmoon76@gmail.com>
2026-08-05 04:07:28 +0600

    verification

--- index.js
+++ index.js
@@ -2,16 +2,100 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+const { initializeApp, cert } = require('firebase-admin/app');
+const { getAuth } = require('firebase-admin/auth');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 // console.log(process.env);
 
-// middleware
+// ===================================================================================
+// for firebase-admin old version:
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./finease-finance-management-firebase-admin-key.json');
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// ===================================================================================
+const serviceAccount = require('./finease-finance-management-firebase-admin-key.json');
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+// ===================================================================================
+// middleware use in everywhere
 app.use(cors());
 app.use(express.json());
 
-// =====================================================================
+// middleware use in only a specific function
+const logger = (req, res, next) => {
+  console.log('logging information');
+  next();
+};
+
+// ===================================================================================
+// for firebase-admin old version:
+// const verifyFireBaseToken = async (req, res, next) => {
+//   console.log('in the verify middleware', req.headers.authorization);
+//   if (!req.headers.authorization) {
+//     // do not allow to go
+//     return res.status(401).send({ message: 'unauthorized access' });
+//   }
+//   const token = req.headers.authorization.split(' ')[1];
+//   if (!token) {
+//     return res.status(401).send({ message: 'unauthorized access' });
+//   }
+
+//   try {
+//     const userInfo = await admin.auth().verifyIdToken(token);
+//     console.log('after token validation', userInfo);
+//     // verify token
+//     next();
+//   } catch {
+//     return res.status(401).send({ message: 'unauthorized access' });
+//   }
+//   // next();
+// };
+
+// ===================================================================================
+// console.log('service', serviceAccount);
+// console.log('admin', admin);
+// console.log('admin credential', admin.credential);
+
+// ===================================================================================
+// for firebase admin version 14
+const verifyFireBaseToken = async (req, res, next) => {
+  // console.log('in the verify middleware', req.headers.authorization);
+  if (!req.headers.authorization) {
+    return res.status(401).send({
+      message: 'Unauthorized access',
+    });
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' });
+  }
+
+  try {
+    const decoded = await getAuth().verifyIdToken(token);
+    // console.log('after token validation', decoded);
+    req.token_email = decoded.email;
+
+    next();
+  } catch (err) {
+    console.log('invalid token');
+    return res.status(401).send({
+      message: 'Unauthorized access',
+    });
+  }
+};
+// ===================================================================================
 // already created in old project name and password:
 // personalfmUser
 // RvYrAGqMqWo0cIGb;
@@ -19,24 +103,68 @@ app.use(express.json());
 // const uri =
 //   'mongodb+srv://personalfmUser:RvYrAGqMqWo0cIGb@cluster0.1daujou.mongodb.net/?appName=Cluster0';
 
-// =====================================================================
-const uri = `mongodb+srv://${process.env.PFM_USER}:${process.env.PFM_PASS}@cluster0.if3njeq.mongodb.net/?appName=Cluster0`;
+// ===================================================================================
+// const uri = `mongodb+srv://${process.env.PFM_USER}:${process.env.PFM_PASS}@cluster0.if3njeq.mongodb.net/?appName=Cluster0`;
 
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+// const uri = `mongodb://${process.env.PFM_USER}:${process.env.PFM_PASS}@ac-uxcxunb-shard-00-00.if3njeq.mongodb.net:27017,ac-uxcxunb-shard-00-01.if3njeq.mongodb.net:27017,ac-uxcxunb-shard-00-02.if3njeq.mongodb.net:27017/?tls=true&replicaSet=atlas-7xhikn-shard-0&authSource=admin&appName=Cluster0`;
 
+// const uri = process.env.MONGODB_URI;
+
+// ================================
+// console.log('server uri:', uri);
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// ===================================================================================
+let client;
+
+async function connectMongo() {
+  const uris = [process.env.MONGODB_URI_SRV, process.env.MONGODB_URI_STANDARD];
+
+  for (const uri of uris) {
+    try {
+      const tempClient = new MongoClient(uri, {
+        serverApi: {
+          version: ServerApiVersion.v1,
+          strict: true,
+          deprecationErrors: true,
+        },
+      });
+
+      await tempClient.connect();
+
+      console.log(
+        `Connected using ${uri.startsWith('mongodb+srv') ? 'SRV' : 'Standard'} URI`,
+      );
+
+      client = tempClient;
+      return;
+    } catch (err) {
+      console.log(
+        `Failed using ${uri.startsWith('mongodb+srv') ? 'SRV' : 'Standard'} URI`,
+      );
+      console.log(err.code || err.message);
+    }
+  }
+
+  throw new Error('Could not connect to MongoDB.');
+}
+
+// ===================================================================================
 app.get('/', (req, res) => {
   res.send('personal finance management Server is running');
 });
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
+    await connectMongo();
 
     const db = client.db('personal_fm');
     const transactionsCollection = db.collection('transactions');
@@ -59,84 +187,16 @@ async function run() {
       }
     });
 
-    // BALANCE APIs
-    // app.get('/balance', async (req, res) => {
-    //   // const projectField = { _id: 0, title: 1, amount: 1 };
-    //   // const cursor = balanceCollection
-    //   //   .find()
-    //   //   .sort({ amount: -1 })
-    //   //   .skip(2)
-    //   //   .limit(2)
-    //   //   .project(projectField);
-
-    //   console.log(req.query);
-    //   const email = req.query.email;
-    //   const query = {};
-    //   if (email) {
-    //     query.email = email;
-    //   }
-
-    //   const cursor = balanceCollection.find(query).limit(3);
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-
-    // app.get('/latest-balance', async (req, res) => {
-    //   const cursor = balanceCollection.find().sort({ amount: 1 });
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-
-    //   // =========================================
-    //   // const email = req.query.email;
-    //   // const query = {};
-    //   // if (email) {
-    //   //   query.email = email;
-    //   // }
-    //   // const cursor = balanceCollection.find(query);
-    //   // const result = await cursor.toArray();
-    //   // res.send(result);
-    // });
-
-    // app.get('/balance/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await balanceCollection.findOne(query);
-    //   res.send(result);
-    // });
-
-    // app.post('/balance', async (req, res) => {
-    //   const newBalance = req.body;
-    //   const result = await balanceCollection.insertOne(newBalance);
-    //   res.send(result);
-    // });
-
-    // app.patch('/balance/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const updatedBalance = req.body;
-    //   const query = { _id: new ObjectId(id) };
-    //   const update = {
-    //     // $set: updatedBalance,
-    //     $set: {
-    //       name: updatedBalance.name,
-    //       amount: updatedBalance.amount,
-    //     },
-    //   };
-    //   const result = await balanceCollection.updateOne(query, update);
-    //   res.send(result);
-    // });
-
-    // app.delete('/balance/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await balanceCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-
+    // ===================================================================================
     // transaction related apis
-    app.get('/transactions', async (req, res) => {
+    app.get('/transactions', logger, verifyFireBaseToken, async (req, res) => {
+      // console.log('headers', req);
       const email = req.query.email;
       const query = {};
       if (email) {
+        if (email !== req.token_email) {
+          return res.status(403).send({ message: 'forbidden access' });
+        }
         query.email = email;
       }
       const cursor = transactionsCollection.find(query).sort({ createdAt: -1 });
@@ -144,12 +204,14 @@ async function run() {
       res.send(result);
     });
 
+    // ===================================================================================
     // app.post('/transactions', async (req, res) => {
     //   const newTransaction = req.body;
     //   const result = await transactionsCollection.insertOne(newTransaction);
     //   res.send(result);
     // });
 
+    // ===================================================================================
     app.post('/transactions', async (req, res) => {
       // const transaction = req.body;
 
@@ -169,7 +231,7 @@ async function run() {
       res.send(result);
     });
 
-    // ============================================================
+    // ===================================================================================
     app.patch('/transactions/:id', async (req, res) => {
       const id = req.params.id;
 
@@ -192,7 +254,7 @@ async function run() {
       res.send(result);
     });
 
-    // ============================================================
+    // ===================================================================================
     app.delete('/transactions/:id', async (req, res) => {
       const id = req.params.id;
 
@@ -203,7 +265,7 @@ async function run() {
       res.send(result);
     });
 
-    // ============================================================
+    // ===================================================================================
     await client.db('admin').command({ ping: 1 });
     console.log(
       'Pinged your deployment. You successfully connected to MongoDB!',
@@ -217,7 +279,7 @@ app.listen(port, () => {
   console.log(`personal finance management server is running on port: ${port}`);
 });
 
-// ====================================================
+// ===================================================================================
 // client
 //   .connect()
 //   .then(() => {
